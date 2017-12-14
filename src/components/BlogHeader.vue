075f11b8c48ae70435785cba8dcb557e7bb67149
lvsongdong <lvsongdong@maka.im>
2017-12-14 14:14:55 +0800

    header

---
+++ src/components/BlogHeader.vue
@@ -0,0 +1,69 @@
+<template>
+  <el-menu
+          :default-active="activeIndex"
+          class="el-menu-demo"
+          mode="horizontal"
+          @select="handleSelect"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#409EFF">
+    <el-menu-item v-for="item in menu" :key="item.id" :index="item.id">{{ item.name }}</el-menu-item>
+  </el-menu>
+</template>
+
+<script>
+export default {
+  name: 'BlogHeader',
+  data () {
+    return {
+      menu: [
+        {
+          'id': '1',
+          'name': '首页',
+          'class': 'bg-purple',
+          'icon': 'el-icon-menu',
+          'link': 'http://garylv.com',
+        },
+        {
+          'id': '2',
+          'name': '博客',
+          'class': 'bg-purple-light',
+          'icon': 'el-icon-tickets',
+          'link': 'http://garylv.com',
+        },
+        {
+          'id': '3',
+          'name': 'GitHub',
+          'class': 'bg-purple',
+          'icon': 'el-icon-star-on',
+          'link': 'http://github.com/garyvv',
+        }
+      ],
+      activeIndex: '1',
+    }
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+        console.log(key, keyPath);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HomePage from '@/components/HomePage'
+
+import HomePage from '@/pages/blog/HomePage'
 
 Vue.use(Router)
 
