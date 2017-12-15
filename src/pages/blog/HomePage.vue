1d3a4720c2279b9fd14812a505f4675f1d5251cc
lvsongdong <lvsongdong@maka.im>
2017-12-15 15:16:16 +0800

    ui

--- src/pages/blog/HomePage.vue
+++ src/pages/blog/HomePage.vue
@@ -14,12 +14,16 @@
       <h1>--- GitHub ---</h1>
       <el-row class="item-list">
         <el-col v-for="card in cards" :key="card.id" :offset="card.id" class="item-list-card">
-          <el-card :body-style="{ padding: '0px', height: '300px', background: card.color, color: '#fff', paddingTop: '10px'}" >
-            <el-tag type="success">{{ card.name }}</el-tag>
-            <div style="padding: 10px;text-align: left">
-              <span>{{ card.content }}</span>
-            </div>
-          </el-card>
+          <el-tooltip class="item" effect="dark" content="打开项目地址" placement="top">
+            <a :href="card.link" target="_blank">
+              <el-card :body-style="{ padding: '0px', height: '300px', background: card.color, color: '#fff', paddingTop: '10px'}" >
+                <el-tag type="success" class="card-tag">{{ card.name }}</el-tag>
+                <div style="padding: 10px;text-align: left">
+                  <span v-html="card.content"></span>
+                </div>
+              </el-card>
+            </a>
+          </el-tooltip>
         </el-col>
       </el-row>
 
@@ -59,39 +63,40 @@ export default {
       cards: [
         {
           'id': 1,
-          'name': 'web-creator 1',
-          'color': '#333',
-          'content': 'laravel 的 一些laravel 的 一些laravel 的 一些laravel 的 一些laravel 的 一些laravel 的 一些laravel 的 一些'
+          'name': 'web-creator',
+          'color': '#EC5f67',
+          'link': 'https://github.com/garyvv/web-creator',
+          'content': '富文本框内容快速生成网页，图片本地化<br />' +
+                      '1、富文本框粘贴网页内容，保存自动生成HTML网页，抓取图片进行下载本地化操作；<br />' +
+                      '2、支持阿里云OSS上传服务<br />' +
+                      '3、可自定义HTML的title、meta，支持SEO优化'
         },
         {
           'id': 2,
-          'name': 'wechat-auth 2',
+          'name': 'wechat-auth',
           'color': '#333',
-          'content': 'jdsalfjdslkafjlasdjf slkdajflsdjafj http://toy.garylv.com/'
+          'link': 'https://github.com/garyvv/wechat-auth',
+          'content': '微信前后端分离登录授权、高拓展性<br />' +
+                      '1、基于PHP laravel 框架开发，使用了easyWeChat库<br />' +
+                      '2、授权、解析服务端进行处理，多终端统一API处理，开发简单<br />' +
+                      '3、公众号配置化自动回复'
         },
         {
           'id': 3,
-          'name': 'oc-api 3',
+          'name': 'opencart',
           'color': '#333',
-          'content': 'jdsalfjdslkafjlasdjf slkdajflsdjafj http://toy.garylv.com/'
+          'link': 'https://github.com/opencart/opencart',
+          'content': 'PHP 国外开源商城（业务开发学习推荐）<br />' +
+                      '1、成熟的电商系统，阅读源码，有助于提升业务开发能力<br />' +
+                      '2、数据库设计，关联数据库设计，规范化的设计和优秀的数据库结构，值得研究学习<br />' +
+                      '3、PHPer提升业务开发值得一阅'
         },
         {
           'id': 4,
-          'name': 'web-creator 4',
+          'name': 'TODO',
           'color': '#333',
-          'content': 'laravel 的 一些laravel 的 一些laravel 的 一些laravel 的 一些laravel 的 一些laravel 的 一些laravel 的 一些'
-        },
-        {
-          'id': 5,
-          'name': 'wechat-auth 5',
-          'color': '#333',
-          'content': 'jdsalfjdslkafjlasdjf slkdajflsdjafj http://toy.garylv.com/'
-        },
-        {
-          'id': 6,
-          'name': 'oc-api 6',
-          'color': '#333',
-          'content': 'jdsalfjdslkafjlasdjf slkdajflsdjafj http://toy.garylv.com/'
+          'link': '/',
+          'content': 'To Be Continue'
         },
       ],
     }
@@ -136,5 +141,15 @@ a {
 .item-list-card {
   width: 25%;
   margin: 20px 4.16%;
+  cursor: pointer;
+}
+.item-list-card :hover {
+  opacity: .8;
+}
+.card-tag {
+  color: #fff;
+  border-color: rgba(255, 255, 255, .5);
+  background-color: rgba(255, 255, 255, .2);
+  font-size: 20px;
 }
 </style>
